a39fb604bfd88d0f1228ff5a7717b33c53696f72
AngelinaDerman <angelina.derman@gmail.com>
2021-03-12 13:47:14 +0300

    feat: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = bracketsConfig.map((aBrackets)=>{
+     return aBrackets.join("");
+   })
+   
+   while(brackets.some((pairBrackets)=>{return str.includes(pairBrackets)})){
+   brackets.forEach(item => {
+   str=str.replace(item,"");
+   });   
+   }
+   if(str.length == 0) {return true;}
+   else {return false;}
+ }
